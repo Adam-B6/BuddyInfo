ec4acf173120324f81ca6d7fc1b4172edc9896b9
abeeh <adambeehler@cmail.carleton.ca>
2023-09-18 11:51:20 -0400

    committing the AddressBook class

---
+++ src/AddressBook.java
@@ -0,0 +1,15 @@
+public class AddressBook {
+
+    BuddyInfo[] buddies;
+
+    public void addBuddy(BuddyInfo buddy) {
+        //add a buddy to the array of buddies
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        //remove a buddy from the array of buddies
+    }
+    public static void main (String[] args) {
+        System.out.println("Address Book");
+    }
+}
