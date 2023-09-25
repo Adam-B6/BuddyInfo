e836d4ea62ddd0077f50ab3e54f66dbde94620d1
abeeh <adambeehler@cmail.carleton.ca>
2023-09-25 11:42:30 -0400

    modified AddressBook added a second buddy

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -23,8 +23,10 @@ public class AddressBook {
     }
     public static void main (String[] args) {
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        BuddyInfo buddy2 = new BuddyInfo("Joe", "OttawaU", "696");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
+        addressBook.addBuddy(buddy2);
         addressBook.removeBuddy(0);
     }
 }
